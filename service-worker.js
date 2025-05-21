c536436a322ec449f92eb084818dc029698c0a31
Paolo Zuccaro <zuccaro.paolo@gmail.com>
2025-05-21 12:48:49 +0200

    Aggiunto caricamento veloce con service worker
    Aggiunto caricamento veloce con service worker

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const VERSION = "v1";
+const CACHE_NAME = `tcgp-shell-${VERSION}`;
+const SHELL = [
+  "./", // la root dell’app
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-1024.png",
+];
+
+// 1) Al momento dell’installazione, pre-cache dell’app shell
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 2) Sul fetch, rispondi sempre con cache-first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
+
+// 3) Al activate, pulisci eventuali cache vecchie
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
